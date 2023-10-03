985c477bf18935513a24bf96e1035e03d917582c
Karlhsiao <56221911+Karlhsiao@users.noreply.github.com>
2023-10-03 11:49:59 +0800

    Add files via upload

---
+++ pythonpythonpython class/w5_hw_order_management_karl_hsiao.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  1 22:54:20 2023
+
+@author: selinayen
+"""
+
+order = [ '編號', '品名', 0, 0, 0 ]
+
+menu = """
+1. 修改訂單號碼
+2. 修改品名
+3. 修改單價
+4. 修改數量
+5. 查詢訂單金額
+請輸入選項1-5，或Q/q離開：
+"""
+
+while True:
+    ans = input(menu)
+    if ans == 'q' or ans == 'Q':
+        break
+    else:
+        if ans == '1':
+            order[0] = input("請輸入要修改的訂單號碼：")
+        elif ans == "2":
+            order[1] = input("請輸入要修改的品名：")
+        elif ans == "3":
+            order[2] = int(input("請輸入要修改的單價："))
+        elif ans == "4":
+            order[3] = int(input("請輸入要修改的數量："))
+        elif ans == "5":
+            print("訂單金額為：", order[4])
+
+    order[4] = order[3] * order[2]
+        #剩下的自己寫寫看喔！
