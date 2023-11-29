1a2c60686e3efb553741ab8879fa68fe7f6bad99
Karlhsiao <56221911+Karlhsiao@users.noreply.github.com>
2023-11-29 17:59:08 +0800

    w4_karl_hsiao_find_num_string.py fixed bug and finished the program

--- pythonpythonpython class/w4_karl_hsiao_find_num_string.py
+++ pythonpythonpython class/w4_karl_hsiao_find_num_string.py
@@ -1,5 +1,25 @@
-input_list = input().split
+input_list = input().split()
 
 ans = ""
 
-print("共同字首為", ans)
+i = 0
+min_len = None
+count = 0
+
+for word in input_list:
+    if min_len == None:
+        min_len = len(word)
+    min_len = min(len(word), min_len)
+
+for char_index in range(min_len):
+    letter_set = set()
+    for word in input_list:
+        letter_set.add(word[char_index])
+
+    if len(letter_set) == 1:
+        count += 1
+    
+    else:
+        break
+
+print("共同字首為", input_list[0][:count])
